a3404c13ed1fa56d4fc6adadfba6cc796f47edc5
Nathan <nathanlamb022@gmail.com>
2017-07-05 17:50:59 +1200

    Try some different conditionals

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
   var frames = [
       [1, 2],
-      [6, 4],
+      [0, 10],
       [5, 4],
       [10, 0],
       [7, 2],
@@ -13,18 +13,73 @@
   var frameTotal = []
   var runningTotal = 0
 
-  for (var i = 0; i < frames.length; i++) {
-      var frameVals = frames[i]
-      for (var j = 0; j < frameVals.length; j++) {
-          var valA = frameVals[0]
-          var valB = frameVals[1]
-          frameTotal[i] = valA + valB
+  for (var i = 0; i < frames.length; i++) { /*Go through frames array */
+      var scores = frames[i] /*scores is whatever frames array index you're up to ie. frames[0] = [1,2] */
+      for (var j = 0; j < scores.length; j++) { /*go through the array i.e frame[0] */
+
+
+
       }
   }
-  for (var k = 0; k < frameTotal.length; k++) {
-      runningTotal += Number(frameTotal[k])
-
-  }
+  console.log(frameTotal)
 
 
-  console.log(runningTotal)
+
+  //   ORIGINAL RUNNING TOTAL
+
+  //     for (var k = 0; k < frames.length; k++) {
+  //         var frameVals = frames[k]
+  //         for (var l = 0; l < frameVals.length; l++) {
+  //             var valA = frameVals[0]
+  //             var valB = frameVals[1]
+  //             frameTotal[k] = valA + valB
+  //         }
+  //     }
+
+  //     for (var k = 0; k < frameTotal.length; k++) {
+  //         runningTotal += Number(frameTotal[k])
+
+  //     }
+
+  //   ORIGINAL CONDITIONAL LOOKING FOR 10
+
+  //     if (scores[1] === 10) { /*if the second number is a 10 then follow this path */
+  //                 var valA = 10 /*valA is the 10 we get from the current frame */
+  //                 var valB = frames[i + 1][0] /*valB is whatever the first number is in the next frame */
+  //                 frameTotal[i] = valA + valB
+  //             } else if (scores[0] === 10) {
+  //                 var valA = 10
+  //                 var valB = frames[i + 1][0] + frames[i + 2][0]
+  //                 frameTotal[i] = valA + valB
+
+  //   ATTEMPT TWO 
+
+  //   if (scores[j] === 10) {
+  //               if (scores[1] === 10) { /*if the second number is a 10 then follow this path */
+  //                   var valA = scores[1] /*valA is the 10 we get from the current frame */
+  //                   var valB = frames[i + 1][0] /*valB is whatever the first number is in the next frame */
+  //                   frameTotal[i] = valA + valB
+  //               } else if (scores[0] === 10) {
+  //                   var valA = scores[0]
+  //                   var valB = frames[i + 1][0] + frames[i + 2][0]
+  //                   frameTotal[i] = valA + valB
+  //               }
+
+  //           } else {
+  //               var valA = scores[0]
+  //               var valB = scores[1]
+  //               frameTotal[i] = valA + valB
+
+  //               if (frameTotal[i] === 10) {
+  //                   var valA = scores[0] + scores[1]
+  //                   var valB = frames[i + 1][0]
+  //                   frameTotal[i] = valA + valB
+  //               } else {
+  //                   var valA = scores[0]
+  //                   var valB = scores[1]
+  //                   frameTotal[i] = valA + valB
+  //               }
+
+  //           }
+
+  //       }
